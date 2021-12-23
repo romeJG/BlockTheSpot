3f1f8162f820a9e359adfe9808e07db18b043220
KUTlime <kutlime@gmail.com>
2021-12-24 00:21:48 +0100

    Update file download with progress bar on non-BitTransfer OS

--- install.ps1
+++ install.ps1
@@ -1,5 +1,59 @@
 # Ignore errors from `Stop-Process`
 $PSDefaultParameterValues['Stop-Process:ErrorAction'] = [System.Management.Automation.ActionPreference]::SilentlyContinue
+function Get-File
+{
+  param (
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [System.Uri]
+    $Uri,
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [System.IO.FileInfo]
+    $TargetFile,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [Int32]
+    $BufferSize = 1,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('KB, MB')]
+    [String]
+    $BufferUnit = 'MB'
+  )
+
+  $request = [System.Net.HttpWebRequest]::Create($Uri)
+  $request.set_Timeout(15000) #15 second timeout
+  $response = $request.GetResponse()
+  $totalLength = [System.Math]::Floor($response.get_ContentLength() / 1024)
+  $responseStream = $response.GetResponseStream()
+  $targetStream = New-Object -TypeName ([System.IO.FileStream]) -ArgumentList "$($TargetFile.FullName)", Create
+  switch ($BufferUnit)
+  {
+    'KB' { $BufferSize = $BufferSize * 1024 }
+    'MB' { $BufferSize = $BufferSize * 1024 * 1024 }
+    Default { $BufferSize = 1024 * 1024 }
+  }
+  Write-Verbose -Message "Buffer size: $BufferSize B ($($BufferSize/("1$BufferUnit")) $BufferUnit)"
+  $buffer = New-Object byte[] $BufferSize
+  $count = $responseStream.Read($buffer, 0, $buffer.length)
+  $downloadedBytes = $count
+
+  while ($count -gt 0)
+  {
+    $targetStream.Write($buffer, 0, $count)
+    $count = $responseStream.Read($buffer, 0, $buffer.length)
+    $downloadedBytes = $downloadedBytes + $count
+    Write-Progress -Activity "Downloading file '$($Uri.ToString().split('/') | Select-Object -Last 1)'" -Status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes / 1024)) / $totalLength) * 100)
+  }
+
+  Write-Progress -Activity "Finished downloading file '$($Uri.ToString().split('/') | Select-Object -Last 1)'"
+
+  $targetStream.Flush()
+  $targetStream.Close()
+  $targetStream.Dispose()
+  $responseStream.Dispose()
+}
 
 Write-Host @'
 *****************
@@ -63,7 +117,6 @@ catch
 }
 
 Write-Host "Downloading latest patch (chrome_elf.zip)...`n"
-$webClient = New-Object -TypeName ([System.Net.WebClient])
 $elfPath = Join-Path -Path $PWD -ChildPath 'chrome_elf.zip'
 try
 {
@@ -74,12 +127,7 @@ try
   }
   else
   {
-    $webClient.DownloadFile(
-      # Remote file URL
-      $uri,
-      # Local file path
-      "$elfPath"
-    )
+    Get-File -Uri $uri -TargetFile "$elfPath"
   }
 }
 catch
@@ -122,12 +170,7 @@ if (-not $spotifyInstalled -or $update)
     }
     else
     {
-      $webClient.DownloadFile(
-        # Remote file URL
-        $uri,
-        # Local file path
-        "$spotifySetupFilePath"
-      )
+      Get-File -Uri $uri -TargetFile "$spotifySetupFilePath"
     }
   }
   catch
