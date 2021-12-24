96980575d2e5ec5cef878e20c334e9c52c4b2ba1
KUTlime <kutlime@gmail.com>
2021-12-24 08:05:59 +0100

    Update to smart-download method resolving

--- install.ps1
+++ install.ps1
@@ -2,57 +2,72 @@
 $PSDefaultParameterValues['Stop-Process:ErrorAction'] = [System.Management.Automation.ActionPreference]::SilentlyContinue
 function Get-File
 {
-  param (
-    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
-    [ValidateNotNullOrEmpty()]
-    [System.Uri]
-    $Uri,
-    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
-    [ValidateNotNullOrEmpty()]
-    [System.IO.FileInfo]
-    $TargetFile,
-    [Parameter(ValueFromPipelineByPropertyName)]
-    [ValidateNotNullOrEmpty()]
-    [Int32]
-    $BufferSize = 1,
-    [Parameter(ValueFromPipelineByPropertyName)]
-    [ValidateNotNullOrEmpty()]
-    [ValidateSet('KB, MB')]
-    [String]
-    $BufferUnit = 'MB'
-  )
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [System.Uri]
+        $Uri,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [System.IO.FileInfo]
+        $TargetFile,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [Int32]
+        $BufferSize = 1,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('KB, MB')]
+        [String]
+        $BufferUnit = 'MB',
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('KB, MB')]
+        [Int32]
+        $Timeout = 10000
+    )
 
-  $request = [System.Net.HttpWebRequest]::Create($Uri)
-  $request.set_Timeout(15000) #15 second timeout
-  $response = $request.GetResponse()
-  $totalLength = [System.Math]::Floor($response.get_ContentLength() / 1024)
-  $responseStream = $response.GetResponseStream()
-  $targetStream = New-Object -TypeName ([System.IO.FileStream]) -ArgumentList "$($TargetFile.FullName)", Create
-  switch ($BufferUnit)
-  {
-    'KB' { $BufferSize = $BufferSize * 1024 }
-    'MB' { $BufferSize = $BufferSize * 1024 * 1024 }
-    Default { $BufferSize = 1024 * 1024 }
-  }
-  Write-Verbose -Message "Buffer size: $BufferSize B ($($BufferSize/("1$BufferUnit")) $BufferUnit)"
-  $buffer = New-Object byte[] $BufferSize
-  $count = $responseStream.Read($buffer, 0, $buffer.length)
-  $downloadedBytes = $count
+    $useBitTransfer = $null -ne (Get-Module -Name BitsTransfer -ListAvailable) -and ($PSVersionTable.PSVersion.Major -le 5)
 
-  while ($count -gt 0)
-  {
-    $targetStream.Write($buffer, 0, $count)
-    $count = $responseStream.Read($buffer, 0, $buffer.length)
-    $downloadedBytes = $downloadedBytes + $count
-    Write-Progress -Activity "Downloading file '$($Uri.ToString().split('/') | Select-Object -Last 1)'" -Status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes / 1024)) / $totalLength) * 100)
-  }
+    if ($useBitTransfer)
+    {
+        Write-Information -MessageData 'Using a fallback BitTransfer method since you are running Windows PowerShell'
+        Start-BitsTransfer -Source $Uri -Destination "$($TargetFile.FullName)"
+    }
+    else
+    {
+        $request = [System.Net.HttpWebRequest]::Create($Uri)
+        $request.set_Timeout($Timeout) #15 second timeout
+        $response = $request.GetResponse()
+        $totalLength = [System.Math]::Floor($response.get_ContentLength() / 1024)
+        $responseStream = $response.GetResponseStream()
+        $targetStream = New-Object -TypeName ([System.IO.FileStream]) -ArgumentList "$($TargetFile.FullName)", Create
+        switch ($BufferUnit)
+        {
+            'KB' { $BufferSize = $BufferSize * 1024 }
+            'MB' { $BufferSize = $BufferSize * 1024 * 1024 }
+            Default { $BufferSize = 1024 * 1024 }
+        }
+        Write-Verbose -Message "Buffer size: $BufferSize B ($($BufferSize/("1$BufferUnit")) $BufferUnit)"
+        $buffer = New-Object byte[] $BufferSize
+        $count = $responseStream.Read($buffer, 0, $buffer.length)
+        $downloadedBytes = $count
+        $downloadedFileName = $Uri -split '/' | Select-Object -Last 1
+        while ($count -gt 0)
+        {
+            $targetStream.Write($buffer, 0, $count)
+            $count = $responseStream.Read($buffer, 0, $buffer.length)
+            $downloadedBytes = $downloadedBytes + $count
+            Write-Progress -Activity "Downloading file '$downloadedFileName'" -Status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes / 1024)) / $totalLength) * 100)
+        }
 
-  Write-Progress -Activity "Finished downloading file '$($Uri.ToString().split('/') | Select-Object -Last 1)'"
+        Write-Progress -Activity "Finished downloading file '$downloadedFileName'"
 
-  $targetStream.Flush()
-  $targetStream.Close()
-  $targetStream.Dispose()
-  $responseStream.Dispose()
+        $targetStream.Flush()
+        $targetStream.Close()
+        $targetStream.Dispose()
+        $responseStream.Dispose()
+    }
 }
 
 Write-Host @'
@@ -68,8 +83,6 @@ Write-Host @'
 Author: @Nuzair46
 *****************
 '@
-
-$useBitTransfer = $null -ne (Get-Module -Name BitsTransfer -ListAvailable) -and ($PSVersionTable.PSVersion.Major -le 5)
 
 $spotifyDirectory = Join-Path -Path $env:APPDATA -ChildPath 'Spotify'
 $spotifyExecutable = Join-Path -Path $spotifyDirectory -ChildPath 'Spotify.exe'
@@ -121,14 +134,7 @@ $elfPath = Join-Path -Path $PWD -ChildPath 'chrome_elf.zip'
 try
 {
   $uri = 'https://github.com/mrpond/BlockTheSpot/releases/latest/download/chrome_elf.zip'
-  if ($useBitTransfer)
-  {
-    Start-BitsTransfer -Source $uri -Destination "$elfPath"
-  }
-  else
-  {
-    Get-File -Uri $uri -TargetFile "$elfPath"
-  }
+  Get-File -Uri $uri -TargetFile "$elfPath"
 }
 catch
 {
@@ -164,14 +170,7 @@ if (-not $spotifyInstalled -or $update)
   try
   {
     $uri = 'https://download.scdn.co/SpotifyFullSetup.exe'
-    if ($useBitTransfer)
-    {
-      Start-BitsTransfer -Source $uri -Destination "$spotifySetupFilePath"
-    }
-    else
-    {
-      Get-File -Uri $uri -TargetFile "$spotifySetupFilePath"
-    }
+    Get-File -Uri $uri -TargetFile "$spotifySetupFilePath"
   }
   catch
   {
