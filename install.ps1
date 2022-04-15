971fe1bca2c8e77657a05968aa561ef819956b64
amd64fox <62529699+amd64fox@users.noreply.github.com>
2022-04-15 16:33:06 +0300

    [skip ci] Fix regex for NavLink button (#362)
    fix regex disable premium NavLink button [1.1.82-1.1.83+]

--- install.ps1
+++ install.ps1
@@ -324,7 +324,7 @@ if ($RemoveAdPlaceholder)
     $xpuiContents = $xpuiContents -replace '\.createElement\([^.,{]+,{(?:spec:[^.,]+,)?onClick:[^.,]+,className:[^.]+\.[^.]+\.UpgradeButton}\),[^.(]+\(\)', ''
 
     # Disable Premium NavLink button
-    $xpuiContents = $xpuiContents -replace '(const|var) .=.\?(`.*?`|"".concat\(.\).concat\(.\)):.;return .\(\)\.createElement\(".",.\(\)\(\{\},.,\{ref:.,href:.,target:"_blank",rel:"noopener nofollow"\}\),.\)', ''
+    $xpuiContents = $xpuiContents -replace 'return (.\(\).createElement\("a".+?"noopener nofollow")', '$1'
 
     if ($fromZip)
     {
